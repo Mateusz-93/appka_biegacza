6350a1dc4fc19e3d1644000536b868b068791e79
Mateusz <mateuszvvilczewski@gmail.com>
2025-06-12 15:11:48 +0200

    poprawki app.py

--- app.py
+++ app.py
@@ -22,11 +22,16 @@ if not st.session_state.get("openai_api_key"):
         if st.session_state["openai_api_key"]:
             st.rerun()
 
+LANGFUSE_PUBLIC_KEY = os.getenv("LANGFUSE_PUBLIC_KEY")
+LANGFUSE_SECRET_KEY = os.getenv("LANGFUSE_SECRET_KEY")
+LANGFUSE_HOST = os.getenv("LANGFUSE_HOST")  
+
 langfuse = Langfuse(
-    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
-    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
-    host=os.getenv("LANGFUSE_HOST", "https://cloud.langfuse.com")
+    public_key=LANGFUSE_PUBLIC_KEY,
+    secret_key=LANGFUSE_SECRET_KEY,
+    host=LANGFUSE_HOST
 )
+
 @observe
 def get_data_from_message_observed(message, model="gpt-4o"):
     prompt = """
